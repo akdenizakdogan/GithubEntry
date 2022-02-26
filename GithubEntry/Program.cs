976bbbdd0d844259aec975535ec9d1fa0421b97b
Akdeniz Akdogan <Akdeniz Akdogan@AKDENIZ>
2022-02-26 03:19:01 +0300

    Add project files.

---
+++ GithubEntry/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubEntry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Github!");
+        }
+    }
+}
